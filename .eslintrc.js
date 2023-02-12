9aeecc69d9471027aee0aadc461f928685938404
Noversal <dionofrio_franco@hotmail.com>
2023-02-12 19:31:52 -0300

    Initializate project

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react','jsx'],
+  rules: {
+    indent: ['error', 2],
+    'linebreak-style': 'off',
+    quotes: ['error', 'single'],
+    semi: 'error',
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off'
+  },
+};
