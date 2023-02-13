0e54c190731f4c9343beafc7d41b3b9837b06111
Noversal <dionofrio_franco@hotmail.com>
2023-02-13 14:21:25 -0300

    Created first Component

---
+++ app/src/components/OptionDolar.jsx
@@ -0,0 +1,19 @@
+function OptionDolar({name, value} ) {
+  return (
+    <>
+      <option value={value}>{name}</option>
+    </>
+  );
+}
+  
+
+export default ({ dolarInfo }) => {
+  return dolarInfo && dolarInfo.map(({ value, name, key }) =>
+    <OptionDolar
+      key={key}
+      value={value}
+      name={name}
+    />
+  );
+};
+  
