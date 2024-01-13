70c2b0db43ee6692d3bd8f9872923da75902d87f
Noversal <dionofrio_franco@hotmail.com>
2024-01-13 20:11:33 -0300

    Icons

---
+++ app/src/components/Arrow.jsx
@@ -0,0 +1,7 @@
+export default function Arrow (props) {
+  return (
+    <svg {...props} viewBox='0 0 24 24' fill='currentColor' xmlns='http://www.w3.org/2000/svg'>
+      <path d='M6 12h12m0 0-5-5m5 5-5 5' stroke='currentColor' strokeWidth='2' strokeLinecap='round' strokeLinejoin='round' />
+    </svg>
+  )
+}
