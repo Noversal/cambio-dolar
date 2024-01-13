cf802455d14248870041783303b086ea4016c049
Noversal <dionofrio_franco@hotmail.com>
2024-01-13 20:11:15 -0300

    Restucturing HTML

--- app/src/components/Form.jsx
+++ app/src/components/Form.jsx
@@ -3,65 +3,63 @@ import { OptionDolar } from './OptionDolar'
 import { useDolar } from '../hooks/useDolar'
 import { useState } from 'react'
 
-export default function Form() {
-    const calcularValor = usePriceStore(state => state.calcularValor)
-    const dolarOptions = useDolar()
-    const [valuePrice, setValuePrice] = useState('')
+export default function Form () {
+  const calcularValor = usePriceStore(state => state.calcularValor)
+  const dolarOptions = useDolar()
+  const [valuePrice, setValuePrice] = useState('')
 
-    const obtenerData = (e) => {
-        e.preventDefault()
-    
-        const values = new FormData(e.currentTarget)
-        let dolarInput = values.get('dolar')
-        if (!dolarInput) {
-            dolarInput = 1
-        }
+  const obtenerData = (e) => {
+    e.preventDefault()
 
-        const option = values.get('valoresDeDolar')
-        calcularValor({ dolarInput, option })
-    }
-    
-    const validateValue = (event) => {
-        const { value } = event.target
-        if ( value.match(/^[1-9]\d*$/) ) {
-            setValuePrice(value)
-        }
+    const values = new FormData(e.currentTarget)
+    let dolarInput = values.get('dolar')
+    if (!dolarInput) {
+      dolarInput = 1
     }
 
-    return (
-    <>
-        <h1 className='calc__title'>
-            <span>Cambio de </span> <span className='green'> Dólares</span>
-        </h1>
-        <form className='form' onSubmit={obtenerData}>
-            <select
-                className='form__valorDolar'
-                name='valoresDeDolar'
-                title='Valores de Dolar'
-            >
-                {
-                    dolarOptions &&
-                    dolarOptions.map(({ value, name, key }) => (
-                        <OptionDolar
-                            key={key}
-                            value={value}
-                            name={name}
-                        />)
-                        )
-                }
-            </select>
-            <label htmlFor='dolar'>Cantidad de Dolares</label>
-            <input
-                name='dolar'
-                className='form__dolarPrice'
-                value={valuePrice}
-                onChange={validateValue}
-                type='text'
-                placeholder='50,100,150,200 ...'
-                title='Cantidad de Dolares'
-            />
-                <button className='form__button'> CALCULAR </button>
-        </form>
-    </>
-    )
+    const option = values.get('valoresDeDolar')
+    calcularValor({ dolarInput, option })
+  }
+
+  const validateValue = (event) => {
+    const { value } = event.target
+    if (value.match(/^[1-9]\d*$/)) {
+      setValuePrice(value)
+    }
+  }
+
+  return (
+    <form className='form' onSubmit={obtenerData}>
+      <select
+        className='form__valorDolar'
+        name='valoresDeDolar'
+        title='Valores de Dolar'
+      >
+        {
+          dolarOptions
+            ? dolarOptions.map(({ value, name, key }) => (
+              <OptionDolar
+                key={key}
+                value={value}
+                name={name}
+              />)
+            )
+            : (
+              <></>)
+        }
+      </select>
+      <label htmlFor='dolar'>Cantidad de Dolares</label>
+      <input
+        id='dolar'
+        name='dolar'
+        className='form__dolarPrice'
+        value={valuePrice}
+        onChange={validateValue}
+        type='text'
+        placeholder='50,100,150,200 ...'
+        title='Cantidad de Dolares'
+      />
+      <button className='button button__primary'> CALCULAR </button>
+    </form>
+  )
 }
--- app/src/pages/Calculate.jsx
+++ app/src/pages/Calculate.jsx
@@ -1,15 +1,18 @@
-import Form from '../components/Form';
-import BoxDolar  from '../components/BoxDolar';
+import Form from '../components/Form'
+import BoxDolar from '../components/BoxDolar'
 
-export default function Calculate() {
+export default function Calculate () {
   return (
-        <main className='calc'>
-          <section className='calc__container'>
-            <Form />
-          </section>
-          <section>
-            <BoxDolar />
-          </section>
-        </main>
-    )
+    <main className='calc'>
+      <section className='calc__container'>
+        <h1 className='calc__title'>
+          <span>Cambio de </span> <span className='green'> Dólares</span>
+        </h1>
+        <Form />
+      </section>
+      <section>
+        <BoxDolar />
+      </section>
+    </main>
+  )
 }
