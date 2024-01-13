c9fd266ae98642375e74e6761c9e2dea9af5f7b4
Noversal <dionofrio_franco@hotmail.com>
2024-01-13 20:14:22 -0300

    Restructuring Results

--- app/src/hooks/useResultPrice.js
+++ app/src/hooks/useResultPrice.js
@@ -1,18 +1,7 @@
-import { useEffect, useState } from 'react';
-import { usePriceStore } from '../store/useStore';
+import { usePriceStore } from '../store/useStore'
 
 export const useResultPrice = () => {
-    const res = usePriceStore(state => state.res)
-    const [cajaRes, setCajaRes] = useState(false)
-    
-    useEffect(() => {
-      if (cajaRes) return 
-      
-      if (res !== '') {
-        setCajaRes(true)
-      }
-      
-    }, [res])
-  
-    return [res, cajaRes, setCajaRes]
-  }
+  const res = usePriceStore(state => state.res)
+
+  return [res]
+}
--- app/src/pages/GridDolar.jsx
+++ app/src/pages/GridDolar.jsx
@@ -1,40 +1,42 @@
-import React, { Suspense, useEffect, useState } from 'react';
-import { getData } from '../service/getData';
+import React, { useEffect, useState } from 'react'
+import { getData } from '../service/getData'
 import { regulationMoney } from '../utils.js'
+import Arrow from '../components/Arrow'
+// import Graph from '../components/Graph.jsx'
 
-export default function GridDolar() {
-  const [value, setValue] = useState(null)
+export default function GridDolar () {
+  const [value, setValue] = useState([])
 
   useEffect(() => {
     getData().then(([data]) => {
       setValue(data)
     })
   }, [])
-  
+
   return (
-    <>
-      {
-        value &&
-        <table>
-          <thead>
-            <tr>
-              <th>Nombre</th>
-              <th>Precio</th>
-            </tr>
-          </thead>
-          <tbody>
-            {value.map(({ name, price }) => {
+    <table>
+      <thead>
+        <tr>
+          <th>Nombre</th>
+          <th> </th>
+          <th>Precio</th>
+        </tr>
+      </thead>
+      <tbody>
+        {
+          value.length !== 0 &&
+            value.map(({ name, price }) => {
               const valueRegular = regulationMoney({ money: price })
               return (
                 <tr key={name}>
                   <td>{name}</td>
-                  <td>$ {valueRegular}</td>
+                  <td> <Arrow className='icon' /> </td>
+                  <td style={{ display: 'flex', gap: '7px', alignItems: 'center' }}>$ {valueRegular} {/* <button> <Graph className='icon' /> </button> */}</td>
                 </tr>
               )
-            })}
-          </tbody>
-        </table>
-      }
-    </>
- );
+            })
+        }
+      </tbody>
+    </table>
+  )
 }
